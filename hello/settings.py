39756976d270a089a950efdd74d63d6f5593ef11
Hgupta-77 <guptaharish8305@gmail.com>
2025-05-16 16:06:43 +0530

    Fix: added domain to ALLOWED_HOSTS

--- hello/settings.py
+++ hello/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-z-h+-61c(ol@*%)7--#bire=%**a=l^z=&9kuhze3vgb8e(n12
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['harish-tour-and-travels.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
