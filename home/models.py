8c3e5a4de381fac86b09c3c098b86e38fdf65a38
Hgupta-77 <guptaharish8305@gmail.com>
2025-05-16 15:03:10 +0530

    Initial commit for Harish Tour and Travels

---
+++ home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import date
+
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField(default=date.today)  # ✅ Fix: Set default to current date
+
+    def __str__(self):
+        return self.name
